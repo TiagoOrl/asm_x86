98723fcb257bf88e85bb9eba31f04fc4fed82b0f
Tiago <tiago_orlando@yahoo.com>
2025-04-29 20:16:58 -0300

    pass more variables in the stack

--- 2_stack_passing_data_functions/main.asm
+++ 2_stack_passing_data_functions/main.asm
@@ -2,21 +2,30 @@
 section .text
 global _start
 
-addTwo:
+addNumbers:
     push ebp ; add ebp return addres
     mov ebp, esp
     mov eax, [ebp + 8] ; "var" a
     mov ebx, [ebp + 12] ; "var" b
     add eax, ebx
+
+    mov ebx, [ebp + 16] ; var c
+    add eax, ebx
+
+    mov ebx, [ebp + 20] ; var d
+    add eax, ebx
+
     pop ebp
     ret
 
 
 
 _start:
-    push 4 ; "var" a
-    push 1 ; "var" b
-    call addTwo ; places the return address on the stack
+    push 4 ; var a
+    push 1 ; var b
+    push 180 ; var c
+    push 30 ; var d
+    call addNumbers ; places the return address on the stack
     mov ebx, eax ; return address here
     mov eax, 1
     int 80h
