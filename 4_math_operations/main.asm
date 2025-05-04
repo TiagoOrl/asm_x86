def2b0aece82696e6b7c8868a684987d7201fa24
Tiago <tiago_orlando@yahoo.com>
2025-05-04 20:36:32 -0300

    feat: division

--- 4_math_operations/main.asm
+++ 4_math_operations/main.asm
@@ -20,7 +20,34 @@ subtract:
     ret
 
 
+; mul: for unsgined multiplications
+; imul: for signed multiplications
+
+multiply:
+    push ebp    
+    mov ebp, esp
+
+    mov al, 0xff ; overflow register example
+    mov bl, 2
+    mul bl ;  multiply the val stored here by the val in al and store in al
+
+
+    pop ebp
+
+
+divide:
+    push ebp    
+    mov ebp, esp
+
+    mov eax, 11
+    mov ecx, 2
+    div ecx ; eax = eax / ecx : remainder is stored in EDX
+
+    
+    pop ebp
+
+
 _start:
-    call subtract
+    call divide
     mov eax, 1
     int 80h
