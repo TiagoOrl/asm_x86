6819b994f893c354e718d7107af59e8ee79483bc
Tiago <tiago_orlando@yahoo.com>
2025-05-05 22:20:44 -0300

    implemented open in buffer

--- 6_syscall_opening_files/main.asm
+++ 6_syscall_opening_files/main.asm
@@ -1,12 +1,23 @@
 section .data
-    pathname DD "/home/tiago/Desktop/text1"
+    path DB "/home/tiago/Desktop/text1", 0
+
+
+section .bss
+    buffer: resb 48
 
 
 section .text
 global _start
 
 _start:   
-    mov eax, 5 ; systemcall for file open
-    mov ebx, pathname ; 1st argument is the pathname 
-    mov ecx, 0 ; 2nd argument is the flags, 0 = ready only
+    MOV eax, 5 ; systemcall for file open
+    MOV ebx, path ; 1st argument is the pathname 
+    MOV ecx, 0 ; 2nd argument is the flags, 0 = ready only
+    INT 80h
+
+    mov ebx, eax
+    mov eax, 3 ; syscall number for file read
+    mov ecx, buffer
+    mov edx, 48
     int 80h
+
